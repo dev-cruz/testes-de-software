aa87ffe9ee9fadee04017356ff857b1b0391d735
dev-cruz <joaovictorcruz82@gmail.com>
2020-09-12 12:10:55 -0300

    estado verde

--- entrega_introducao/src/main/java/entrega_introducao/Converter.java
+++ entrega_introducao/src/main/java/entrega_introducao/Converter.java
@@ -1,7 +1,26 @@
 package entrega_introducao;
 
 public class Converter {
-	public Double convertToDollar(Double value, String currency) {
-		throw new IllegalArgumentException();
+	public double convertToDollar(double value, String currency) throws Exception {
+		double convertedDollar;
+		
+		switch(currency) {
+		case "BRL":
+			convertedDollar = value * 0.19;
+			break;
+			
+		case "EUR":
+			convertedDollar = value * 1.18;
+			break;
+			
+		case "JPY":
+			convertedDollar = value * 0.0094;
+			break;
+			
+		default: 
+			throw new Exception("Currency not available");		
+		}
+		
+		return Math.round(convertedDollar * 10.0) / 10.0;
 	}
 }
--- entrega_introducao/src/test/java/entrega_introducao/LibraryTest.java
+++ entrega_introducao/src/test/java/entrega_introducao/LibraryTest.java
@@ -3,14 +3,27 @@
  */
 package entrega_introducao;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
 	Converter converter = new Converter();
 	
-	@Test public void testSomeLibraryMethod() {
+	@Test public void convertingRealToDollar() throws Exception {
 		// Double.compare returns 0 if the two values are numerically equals
-        assertEquals(Double.compare(converter.convertToDollar(1.0, "BRL"), 0.19), 0);
+        assertEquals(Double.compare(converter.convertToDollar(1.0, "BRL"), 0.2), 0);
     }
+	
+	@Test public void convertingEuroToDollar() throws Exception {
+		// Double.compare returns 0 if the two values are numerically equals
+        assertEquals(Double.compare(converter.convertToDollar(5.0, "EUR"), 5.9), 0);
+    }
+	
+	@Test public void testExpectedException() {
+		Exception exception = assertThrows(Exception.class, () -> {
+			converter.convertToDollar(10.0, "ABC");
+		});
+		
+		assertEquals(exception.getMessage(), "Currency not available");
+	}
 }
