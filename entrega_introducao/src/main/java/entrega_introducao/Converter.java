a16b78145dbbe80cb39167a4e9f0666ed385d6cf
dev-cruz <joaovictorcruz82@gmail.com>
2020-09-14 10:27:46 -0300

    estado azul

--- entrega_introducao/src/main/java/entrega_introducao/Converter.java
+++ entrega_introducao/src/main/java/entrega_introducao/Converter.java
@@ -1,26 +1,18 @@
 package entrega_introducao;
 
+import java.util.Map;
+
 public class Converter {
 	public double convertToDollar(double value, String currency) throws Exception {
 		double convertedDollar;
+		Map<String, Double> currencyMultiplier = Map.of("BRL", 0.19, "EUR", 1.18, "JPY", 0.0094);
 		
-		switch(currency) {
-		case "BRL":
-			convertedDollar = value * 0.19;
-			break;
-			
-		case "EUR":
-			convertedDollar = value * 1.18;
-			break;
-			
-		case "JPY":
-			convertedDollar = value * 0.0094;
-			break;
-			
-		default: 
-			throw new Exception("Currency not available");		
+		if (currencyMultiplier.containsKey(currency)) {
+			Double multiplier = currencyMultiplier.get(currency);
+			convertedDollar = value * multiplier;
+			return Math.round(convertedDollar * 10.0) / 10.0;
+		} else {
+			throw new Exception("Currency not available");
 		}
-		
-		return Math.round(convertedDollar * 10.0) / 10.0;
 	}
 }
