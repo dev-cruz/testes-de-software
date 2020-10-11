23597f88b1abd973446b0d0f99685eef2939116c
dev-cruz <joaovictorcruz82@gmail.com>
2020-10-11 13:51:02 -0300

    added atividade_pratica_01 with java money dependency

---
+++ entregas_testes_funcionais/atividade_pratica_01/src/main/java/atividade_pratica_01/Converter.java
@@ -0,0 +1,18 @@
+package atividade_pratica_01;
+
+import java.util.Map;
+
+public class Converter {
+	public double convertToDollar(double value, String currency) throws Exception {
+		double convertedDollar;
+		Map<String, Double> currencyMultiplier = Map.of("BRL", 0.19, "EUR", 1.18, "JPY", 0.0094);
+		
+		if (currencyMultiplier.containsKey(currency)) {
+			Double multiplier = currencyMultiplier.get(currency);
+			convertedDollar = value * multiplier;
+			return Math.round(convertedDollar * 10.0) / 10.0;
+		} else {
+			throw new Exception("Currency not available");
+		}
+	}
+}
